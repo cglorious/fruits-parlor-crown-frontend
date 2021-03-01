733d1bec278b5babe894f35240f0380e9e374bef
Catherine Gloria <catherinegloria@gmail.com>
2021-03-01 12:33:03 -0800

    create character profile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const CATEGORIES_URL = `http://localhost:3000/api/v1/categories`
 const CHARACTERS_URL = `http://localhost:3000/api/v1/characters`
+const IMAGE_URL = `https://ih1.redbubble.net/image.361175264.4945/st,small,845x845-pad,1000x1000,f8f8f8.u5.jpg`
 
 document.addEventListener('DOMContentLoaded', () => {
   loadCategories()
@@ -46,6 +47,53 @@ function changeHeader(option){
 }
 
 function renderCharacter(char){
+  const container = document.getElementById('character-container')
+
+  //create nodes
+  const card = document.createElement('div')
+  const row = document.createElement('div')
+  const col4 = document.createElement('div')
+  const img = document.createElement('img')
+  const col8 = document.createElement('div')
+  const cardBody = document.createElement('div')
+  const h5 = document.createElement('h5')
+  const bio = document.createElement('p')
+  const power = document.createElement('p')
+  const afn = document.createElement('p')
+  const smallPower = document.createElement('small')
+  const smallAfn = document.createElement('small')
+
+  //set attributes
+  card.setAttribute('class','card mb-3')
+  row.setAttribute('class','row no-gutters')
+  col4.setAttribute('class','col-md-4')
+  img.setAttribute('class','card-img')
+  img.setAttribute('alt',`${char.name}`)
+  img.setAttribute('src', IMAGE_URL)
+  col8.setAttribute('class','col-md-8')
+  cardBody.setAttribute('class','card-body')
+  h5.setAttribute('class','card-title')
+  bio.setAttribute('class','card-text')
+  power.setAttribute('class','card-text')
+  power.setAttribute('style','font-style: italic')
+  afn.setAttribute('class','card-text')
+  smallAfn.setAttribute('class', 'text-muted')
+
+  //set inner content
+  h5.textContent = `${char.name}, ${char.title}`
+  bio.textContent = `${char.bio}`
+  smallPower.textContent = `${char.power}`
+  smallAfn.textContent = `${char.affiliation}`
+
+  //append nodes
+  col4.append(img)
+  power.append(smallPower)
+  afn.append(smallAfn)
+  cardBody.append(h5, bio, power, afn)
+  col8.append(cardBody)
+  row.append(col4, col8)
+  card.append(row)
+  container.append(card)
   console.log(char.name)
 }
 
