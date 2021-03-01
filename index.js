de328bfa85d8066e698aabb302659d9fd358e1a8
Catherine Gloria <catherinegloria@gmail.com>
2021-03-01 10:57:12 -0800

    load options in dropdown

---
+++ index.js
@@ -0,0 +1,31 @@
+const CATEGORIES_URL = `http://localhost:3000/api/v1/categories`
+const CHARACTERS_URL = `http://localhost:3000/api/v1/characters`
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadCategories()
+})
+
+function loadCategories(){
+  fetch(CATEGORIES_URL)
+  .then(resp => resp.json())
+  .then(json => {
+    json.data.forEach(category => loadOptions(category));
+  })
+}
+
+function loadOptions(option){
+  const dropdown = document.getElementById('dropdown-menu')
+  const btn = document.createElement('button')
+
+  btn.setAttribute('id', option.id)
+  btn.setAttribute('class', "dropdown-item")
+  btn.setAttribute('type', 'button')
+  btn.innerText = option.attributes.name
+  btn.addEventListener("click", (e) => {fetchCharacter(e.target.id)});
+
+  dropdown.append(btn)
+}
+
+function fetchCharacter(num){
+  console.log(num)
+}
