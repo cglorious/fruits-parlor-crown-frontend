f3cfe4119834293ed0c129836fd25be7bbc1e02a
Catherine Gloria <catherinegloria@gmail.com>
2021-03-01 11:17:47 -0800

    option header

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function loadOptions(option){
   btn.setAttribute('id', option.id)
   btn.setAttribute('class', "dropdown-item")
   btn.setAttribute('type', 'button')
-  btn.innerText = option.attributes.name
+  btn.innerText = `${option.attributes.name}s`
   btn.addEventListener("click", (e) => {fetchCharacters(e.target.id)});
 
   dropdown.append(btn)
@@ -30,6 +30,37 @@ function fetchCharacters(value){
   fetch(`${CATEGORIES_URL}/${value}`)
   .then(resp => resp.json())
   .then(json => {
-  console.log(json.data);
+  optionHeader(json.data);
 })
+}
+
+function optionHeader(option){
+  const header = document.getElementById('option-header')
+  const jumbotron = document.createElement('div')
+  const container = document.createElement('div')
+  const h1 = document.createElement('h1')
+  const p = document.createElement('p')
+
+  jumbotron.setAttribute('class', 'jumbotron jumbotron-fluid')
+  container.setAttribute('class', 'container')
+  h1.setAttribute('class', 'display-4')
+  p.setAttribute('class', 'lead')
+
+  h1.textContent = `The ${option.attributes.name}s`
+  p.textContent = `Here is a list of known ${option.attributes.name}s.`
+
+  container.append(h1, p)
+  jumbotron.append(container)
+  header.append(jumbotron)
+
+  console.log(option.attributes.name)
+  // const div = document.getElementById('chart')
+  // const h2 = document.createElement('h2')
+  // const jobs = chart.attributes.jobs
+  //
+  // h2.setAttribute('class', 'chart-title')
+  // h2.innerText = `${chart.attributes.name} Chart`
+  // div.append(h2)
+  //
+  // renderJobs(jobs)
 }
